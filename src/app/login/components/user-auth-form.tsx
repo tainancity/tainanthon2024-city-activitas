7a8720f01d29c89e89e479c8c40f2f336a89c653
cshif <5175166+cshif@users.noreply.github.com>
2024-10-26 14:57:40 +0800

    feat: add login page

---
+++ src/app/login/components/user-auth-form.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { Icons } from '@/components/ui/icons';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  return (
+    <div className={cn('grid gap-6', className)} {...props}>
+      <div className="grid gap-2">
+        <div className="grid gap-1">
+          <Label className="sr-only" htmlFor="email">
+            Email
+          </Label>
+          <Input
+            id="email"
+            placeholder="name@example.com"
+            type="email"
+            autoCapitalize="none"
+            autoComplete="email"
+            autoCorrect="off"
+            disabled={isLoading}
+          />
+        </div>
+        <Button
+          disabled={isLoading}
+          onClick={() => {
+            router.push('/admin');
+          }}
+        >
+          {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
+          登入
+        </Button>
+      </div>
+    </div>
+  );
+}
