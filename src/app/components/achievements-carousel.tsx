3ed5af15a68e5c637e3fac3ac162baaf40c9560b
cshif <5175166+cshif@users.noreply.github.com>
2024-11-09 16:45:59 +0800

    feat: add home achievements carousel

---
+++ src/app/components/achievements-carousel.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Autoplay from 'embla-carousel-autoplay';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+
+export const AchievementsCarousel = () => (
+  <Carousel
+    plugins={[Autoplay({ delay: 2000 })]}
+    className="w-full max-w-screen-xl"
+  >
+    <CarouselContent>
+      {Array.from({ length: 5 }).map((_, index) => (
+        <CarouselItem key={index}>
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-video items-center justify-center p-6">
+                <span className="text-4xl font-semibold">{index + 1}</span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      ))}
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext />
+  </Carousel>
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from '@/app/components/navbar';
 import { Search } from '@/app/components/search';
+import { AchievementsCarousel } from '@/app/components/achievements-carousel';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
     <>
       <div className="hidden flex-col md:flex">
         <div className="border-b">
-          <div className="flex h-16 items-center px-4">
+          <div className="flex h-16 items-center px-4 max-w-screen-xl mx-auto">
             <div className="mx-4">
               <h1 className="text-2xl font-bold">CityActivitas</h1>
             </div>
@@ -18,10 +19,13 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex-1 space-y-4 p-8 pt-6 mx-auto max-w-screen-xl min-w-[1280px]">
           <div className="flex items-center justify-between space-y-2">
-            {/* TODO main content */}
+            <AchievementsCarousel />
           </div>
+        </div>
+        <div className="flex-1 space-y-4 p-8 pt-6 mx-auto max-w-screen-xl min-w-[1280px]">
+          <h2 className="text-2xl font-bold">找空間</h2>
         </div>
       </div>
     </>
