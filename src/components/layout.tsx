62db43a13937f308573eade008433113b7a9d7fd
cshif <5175166+cshif@users.noreply.github.com>
2024-11-19 14:54:34 +0800

    feat: add public layout

---
+++ src/components/layout.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { Navbar } from '@/app/components/navbar';
+import { Search } from '@/app/components/search';
+import { Button } from '@/components/ui/button';
+import { Footer } from '@/app/components/footer';
+import { useRouter } from 'next/navigation';
+
+export default function Layout({ children }: ReactNode) {
+  const router = useRouter();
+
+  return (
+    <div className="hidden flex-col md:flex min-h-screen">
+      <main className="flex-grow flex flex-col">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4 max-w-screen-xl mx-auto">
+            <div className="mx-4">
+              <h1 className="text-2xl font-bold">CityActivitas</h1>
+            </div>
+            <Navbar />
+            <div className="ml-auto flex items-center space-x-4">
+              <Search />
+              <Button
+                onClick={() => {
+                  router.push('/');
+                }}
+              >
+                我想提案
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="flex-grow">{children}</div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
