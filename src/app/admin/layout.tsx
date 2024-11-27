6a08fbeb84d057458afb45bf099b8f0be183d6b0
cshif <5175166+cshif@users.noreply.github.com>
2024-11-27 14:09:59 +0800

    feat: update admin layout style

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AdminSidebar />
-      <main className="p-4" style={{ minWidth: 'calc(100% - 256px)' }}>
+      <main className="bg-gray-100" style={{ minWidth: 'calc(100% - 256px)' }}>
         {/*<SidebarTrigger />*/}
         {children}
       </main>
