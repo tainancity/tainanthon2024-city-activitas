dae0b424ee75b645d4e588b5124439fc31fa75e9
cshif <5175166+cshif@users.noreply.github.com>
2024-11-27 13:43:27 +0800

    feat: modify admin layout min-width

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AdminSidebar />
-      <main className="p-4">
+      <main className="p-4" style={{ minWidth: 'calc(100% - 256px)' }}>
         {/*<SidebarTrigger />*/}
         {children}
       </main>
