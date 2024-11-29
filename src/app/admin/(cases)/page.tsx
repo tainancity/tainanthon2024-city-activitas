0713687eef4786a2ed3ae23b9ed2955e5e419d38
cshif <5175166+cshif@users.noreply.github.com>
2024-11-29 15:43:38 +0800

    fix: remove non-exist component and import

--- src/app/admin/(cases)/page.tsx
+++ src/app/admin/(cases)/page.tsx
@@ -1,13 +1,9 @@
-import {
-  CaseTrackingPage,
-  AssetCases,
-} from '@/app/admin/components/case-tracking-page';
+import { CaseTrackingPage } from '@/app/admin/components/case-tracking-page';
 
 export default function Page() {
   return (
     <>
       <CaseTrackingPage />
-      {/* <AssetCases /> */}
     </>
   );
 }
